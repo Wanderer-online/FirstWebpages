bae667cd1fb20c3119acc96b92eb2a515d725aa9
Александр Бродяной <8estworker@gmail.com>
2022-06-25 23:53:46 +0300

    ДЗ по добавлению обработчиков событий

--- java-script/movieDB_homework/js/script.js
+++ java-script/movieDB_homework/js/script.js
@@ -44,7 +44,8 @@ let filmsListItems = document.getElementsByClassName("promo__interactive-item");
 
 for (let i = 0; i < movieDB.movies.length; i++) {
   // console.log(filmsListItems[i]);
-  filmsListItems[i].innerHTML = `${i + 1} ${movieDB.movies[i]}`;
+  filmsListItems[i].innerHTML =
+    `${i + 1} ${movieDB.movies[i]}` + "<div class='delete'></div>";
 }
 // document.querySelector(".promo__interactive-list").replaceWith("");
 
@@ -73,3 +74,82 @@ for (let i = 0; i < movieDB.movies.length; i++) {
 //     </li>
 //     `;
 // });
+
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" -
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение:
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+// Возьмите свой код из предыдущей практики
+
+const inp = document.querySelector(".adding__input");
+const formContainer = document.querySelector(".add");
+const btn = formContainer.lastElementChild;
+let chkBox = "";
+for (let elem of formContainer.childNodes) {
+  if (elem.type == "checkbox") {
+    chkBox = elem;
+    break;
+  }
+  // console.log(elem.type);
+}
+const delBtn = document.querySelectorAll(".delete");
+
+// const chkBox = document.querySelector("checkbox");
+console.log(chkBox);
+
+//добавление в базу новых фильмов + обрезка до 21 символа
+btn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let userFilm = inp.value;
+  // console.log(inp.value);
+  if (userFilm.length > 21) {
+    userFilm = userFilm.slice(0, 21) + "...";
+  }
+
+  if (chkBox.checked == true) {
+    console.log("favorite");
+  }
+
+  movieDB.movies.push(userFilm);
+  movieDB.movies.sort();
+  console.log(movieDB.movies);
+});
+
+
+//удаление фильмов по нажатию на корзину (появляется при наведении на название)
+console.log(delBtn);
+delBtn.forEach((elem) => {
+  elem.addEventListener("click", function (event) {
+    // console.log(elem.parentElement);
+    elem.parentElement.remove();
+  });
+});
+
+// let res = serchInDB(inp.value);
+//   if (res!= false){
+//     res.remove();
+//   }
+function serchInDB(str) {
+  movieDB.movies.forEach((elem) => {
+    if (elem == str) {
+      console.log(elem);
+      return elem;
+    } else {
+      console.log("not found");
+      return false;
+    }
+  });
+}
